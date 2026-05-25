dda91990e902a2222dd76a82eb7503f824174c73
Celivra <52xiaozhai@gmail.com>
2026-05-25 09:21:10 +0800

    minor adjustment for user entity

--- src/main/java/com/celivra/bbs/Entity/User.java
+++ src/main/java/com/celivra/bbs/Entity/User.java
@@ -2,6 +2,8 @@ package com.celivra.bbs.Entity;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class User {
     private Integer id;
@@ -9,21 +11,24 @@ public class User {
     private String phone;
     private String username;
     private String password;
+    private LocalDateTime createTime;
 
     public User() { }
 
-    public User(Integer id, String email, String phone, String username, String password) {
+    public User(Integer id, String email, String phone, String username, String password,  LocalDateTime createTime) {
         this.id = id;
         this.email = email;
         this.phone = phone;
         this.username = username;
         this.password = password;
+        this.createTime = createTime;
     }
 
-    public User(String email, String phone, String username, String password) {
+    public User(String email, String phone, String username, String password,  LocalDateTime createTime) {
         this.email = email;
         this.phone = phone;
         this.username = username;
         this.password = password;
+        this.createTime = createTime;
     }
 }
